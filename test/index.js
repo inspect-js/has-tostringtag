0228f315577999f9297569373d6d4639f47ebecd
Jordan Harband <ljharb@gmail.com>
2026-03-15 22:52:19 -0700

    [Tests] increase coverage

--- test/index.js
+++ test/index.js
@@ -2,6 +2,7 @@
 
 var test = require('tape');
 var hasSymbolToStringTag = require('../');
+var hasSymbolToStringTagShams = require('../shams');
 var runSymbolTests = require('./tests');
 
 test('interface', function (t) {
@@ -17,5 +18,21 @@ test('Symbol.toStringTag exists', { skip: !hasSymbolToStringTag() }, function (t
 
 test('Symbol.toStringTag does not exist', { skip: hasSymbolToStringTag() }, function (t) {
 	t.equal(typeof Symbol === 'undefined' ? 'undefined' : typeof Symbol.toStringTag, 'undefined', 'global Symbol.toStringTag is undefined');
+	t.end();
+});
+
+test('shams interface', function (t) {
+	t.equal(typeof hasSymbolToStringTagShams, 'function', 'is a function');
+	t.equal(typeof hasSymbolToStringTagShams(), 'boolean', 'returns a boolean');
+	t.end();
+});
+
+test('shams: Symbol.toStringTag exists', { skip: !hasSymbolToStringTagShams() }, function (t) {
+	t.equal(hasSymbolToStringTagShams(), true, 'shams returns true when Symbol.toStringTag exists');
+	t.end();
+});
+
+test('shams: Symbol.toStringTag does not exist', { skip: hasSymbolToStringTagShams() }, function (t) {
+	t.equal(hasSymbolToStringTagShams(), false, 'shams returns false when Symbol.toStringTag does not exist');
 	t.end();
 });
